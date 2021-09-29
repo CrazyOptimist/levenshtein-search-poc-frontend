7d84eb22e8b35a4dbbd3026f1a621853c55b3b81
CrazyOptimist <crazyoptimist@mail.com>
2021-09-29 18:20:41 -0500

    remove react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import './App.css'
 
 import ReportCardContainer from './components/reportCardContainer/reportCardContainer'
 
-const queryClient = new QueryClient()
-
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="bg-cover w-screen h-screen app-body">
-        <section className="text-gray-700 body-font border-t border-gray-200">
-          <div className="container px-5 py-5 mx-auto">
-            <div className="flex flex-col text-center w-full mb-10">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mt-10">
-                Search Reports
-              </h1>
-            </div>
-            <ReportCardContainer />
+    <div className="bg-cover w-screen h-screen app-body">
+      <section className="text-gray-700 body-font border-t border-gray-200">
+        <div className="container px-5 py-5 mx-auto">
+          <div className="flex flex-col text-center w-full mb-10">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mt-10">
+              Search Reports
+            </h1>
           </div>
-        </section>
-      </div>
-    </QueryClientProvider>
+          <ReportCardContainer />
+        </div>
+      </section>
+    </div>
   )
 }
 
